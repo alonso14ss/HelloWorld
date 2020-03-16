301d332d6f665f876bf9481ab3fbd0ee6a579f15
alonso14ss <reiyan92@gmail.com>
2020-03-16 15:39:44 +0900

    텍스트뷰까지 연습

--- app/src/main/java/co/kr/tjoeun/helloworld/bsh/MainActivity.java
+++ app/src/main/java/co/kr/tjoeun/helloworld/bsh/MainActivity.java
@@ -9,9 +9,12 @@ public class MainActivity extends AppCompatActivity {
 //    첫 프로젝트 주석
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
